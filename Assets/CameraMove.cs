2f412fee3a3d104e208c5e2dfe4a3d19d79664e3
Zachary Hamilton-Bridge <zachbridge@hotmail.com>
2017-05-31 08:54:44 +0000

    README.md edited online with Bitbucket

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    public LayerMask groundlayer;
+
+    [System.Serializable]
+    public class PositionSettings
+    {
+        public bool invertPan = true;
+        public float panSmooth = 7;
+        public float distanceFromGround = 40;
+        public bool allowZoom = true;
+        public float zoomSmooth = 5;
+        public float zoomStep = 5;
+        public float maxZoom = 25;
+        public float minZoom = 80;
+
+        [HideInInspector]
+        public float newDistance = 40;
+
+        
+
+    }
+
+    [System.Serializable]
+    public class OrbitSettings
+    {
+        public float xRotation = 50;
+        public float yRotation = 0;
+        public bool allowYOrbit = true;
+        public float yOrbitSmooth = 0.5f;
+
+    }
+
+    [System.Serializable]
+    public class InputSettings
+    {
+        public string PAN = "MousePan";
+        public string ORBIT_Y = "MouseTurn";
+        public string ZOOM = "Mouse ScrollWheel";
+
+    }
+
+    public PositionSettings position = new PositionSettings;
+    public OrbitSettings orbit = new OrbitSettings;
+    public InputSettings input = new InputSettings;
+
+    Vector3 destination = Vector3.zero;
+    Vector3 camVel = Vector3.zero;
+    Vector3 previousMousePos = Vector3.zero;
+    Vector3 currentMousePos = Vector3.zero;
+    float panInput, orbitInput, zoomInput;
+    int panDirection = 0;
+
+    void Start()
+    {
+        // initialisation code 
+
+    }
+
+
+    void getInput()
+    {
+        // responsible  for setting our input variables 
+    }
+
+    void Update()
+    {
+        //upodating input, zooming, rotating, panning 
+
+    }
+
+    void FixedUpdate()
+    {
+        // handle camera distance -- raycasting goes in fixed update 
+
+    }
+
+    void PanWorld()
+    {
+
+
+    }
+
+    void handleCameraDistance()
+    {
+
+
+    }
+
+    void Zoom()
+    {
+
+
+    }
+
+    void Rotate()
+    {
+
+
+    }
+
+
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+# README #
+
+This README would normally document whatever steps are necessary to get your application up and running.
+
+### What is this repository for? ###
+
+*** Quick summary *** 
+
+Slow-moving side project with the aim to re-create an old Warcraft 3 and Starcraft 2 Custom Game, Hero Line Wars.
+
+
+*** Version ***
+
+v0.01
+
+* [Learn Markdown](https://bitbucket.org/tutorials/markdowndemo)
+
+### How do I get set up? ###
+
+* Summary of set up
+* Configuration
+* Dependencies
+* Database configuration
+* How to run tests
+* Deployment instructions
+
+### Contribution guidelines ###
+
+* Writing tests
+* Code review
+* Other guidelines
+
+### Who do I talk to? ###
+
+* Repo owner or admin
+* Other community or team contact
