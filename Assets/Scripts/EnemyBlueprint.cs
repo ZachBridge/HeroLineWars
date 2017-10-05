d7ad56035c4cb7ceb2182c84e285e700d6f8d292
Declan Holmes-Carr <29772947+Enoai@users.noreply.github.com>
2017-10-05 10:59:42 +0100

    partial script Update

---
+++ Assets/Scripts/EnemyBlueprint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//simple script to whichs just contains a few variables to which are used in other classes (build manager etc)
+[System.Serializable]
+public class EnemyBlueprint
+{
+    //Maybe attach a range indicator to a turret blueprint as a seperate object.
+
+    public GameObject prefab;
+    public int cost;
+
+    public GameObject upgradedPrefab;
+    public int upgradeCost;
+
+}
